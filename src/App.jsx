f82d2bf7496f84bf71b527910d7cd4aa5dae7b6c
LimoHezron <kibungeihezron@gmail.com>
2025-10-01 12:29:23 +0300

    Routing Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { Routes,Route } from "react-router-dom"
+import Home from "./Pages/Home/Home"
+import About from './Pages/About/About'
 
 const App = () => {
   return (
-    <></>
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>} />
+    </Routes>
+    </>
   )
 }
 
